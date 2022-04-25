de248b6f76c3e7eca6f5908f10a830d69c2df9f5
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-04-25 13:29:16 -0600

    removed try except

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import requests
 
 rhead = None
 
-try:
-    rhead = requests.head("https://docsdb.cs.ualberta.ca/Timetable/1780.html", verify = False)
-except:
-    print("Warning: Unsecure")
+rhead = requests.head("https://docsdb.cs.ualberta.ca/Timetable/1780.html", verify = False)
 
-print(rhead)
+
+with open("etag.txt") as etag:
+    print(rhead.headers["Etag"])
