11f99f61c763ff37d43fe0f4a31ef548d8a53e53
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-04-25 15:21:58 -0600

    Will now change etag if not the same as stored value

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from turtle import update
+from unittest import skip
 import requests
 
-rhead = None
+
+def updatedb():
+    pass
 
 rhead = requests.head("https://docsdb.cs.ualberta.ca/Timetable/1780.html", verify = False)
 
-
 with open("etag.txt") as etag:
-    print(rhead.headers["Etag"])
+    if(rhead.headers["Etag"] != etag.readline().strip("\n")):
+        update = True
+
+if update:
+    with open("etag.txt", "w") as etag:
+        etag.write(rhead.headers["Etag"])
+
+    updatedb()
