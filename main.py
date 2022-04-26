4d5167414116448e375134ef134441f5f3d9d6a3
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-04-25 20:59:23 -0600

    saving before I restart

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from turtle import update
-from unittest import skip
+from bs4 import BeautifulSoup as BS
 import requests
 
 
 def updatedb():
-    pass
+    #content = requests.get("https://docsdb.cs.ualberta.ca/Timetable/1780.html", verify = False).text
+    content = open("content.txt", "r").read()
+    BScontent = BS(content,"html.parser")
+    courses = BScontent.find_all("h2")
+    enroll = BScontent.find_all("table")
+    print(enroll[1])
+    return
 
 rhead = requests.head("https://docsdb.cs.ualberta.ca/Timetable/1780.html", verify = False)
-
+update = False
 with open("etag.txt") as etag:
     if(rhead.headers["Etag"] != etag.readline().strip("\n")):
         update = True
@@ -16,4 +21,4 @@ if update:
     with open("etag.txt", "w") as etag:
         etag.write(rhead.headers["Etag"])
 
-    updatedb()
+updatedb() #make sure to tab this so it only updates when needed
