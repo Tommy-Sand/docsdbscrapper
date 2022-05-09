512bb351e75cdfead04edb2ee98d9573aefd5676
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-05-09 08:39:04 -0600

    Added try except and proper html file

--- enrollmentgrapher/graphingsite/views.py
+++ enrollmentgrapher/graphingsite/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import semester
 # Create your views here.
 def index(request):
-    return HttpResponse("fgh")
+    try:
+        semesters_list = [i.sem_name[26:len(i.sem_name)-7] for i in semester.objects.all()]
+    except:
+        raise Http404("Semesters could not be found")
+    semesters_list.insert(0, "<Please pick a semester>")
+    template = loader.get_template('index.html')
+    context = {'Semesters': semesters_list}
+    return HttpResponse(template.render(context, request))
