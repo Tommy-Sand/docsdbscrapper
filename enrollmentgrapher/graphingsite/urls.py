b6abd9eb3618445deee042d0b0c7b64ab0ad95eb
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-05-21 17:41:22 -0600

    Added a path when you submit for a specific semester

--- enrollmentgrapher/graphingsite/urls.py
+++ enrollmentgrapher/graphingsite/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from . import views
 
+app_name = "graphingsite"
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('submit/', views.submit, name='submit')
 ]
