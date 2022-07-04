d6dbf539807adee4ffa3551ee059f941a7c32c2e
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-04 12:42:46 -0600

    Added a complete path to go from the semester to lecture graph

--- enrollmentgrapher/graphingsite/urls.py
+++ enrollmentgrapher/graphingsite/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "graphingsite"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('submit/', views.submit, name='submit')
+    path("submit_s/", views.submit_s, name = "submit_s"),
+    path("submit_s_c/", views.submit_s_c, name = "submit_s_c"),
+    path("submit_s_c_l/", views.submit_s_c_l, name= "submit_s_c_l")
 ]
--- enrollmentgrapher/graphingsite/views.py
+++ enrollmentgrapher/graphingsite/views.py
@@ -1,8 +1,9 @@
 
+from tkinter.colorchooser import Chooser
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.template import loader
-from .models import semester
+from .models import lecture, semester
 import json
 from datetime import datetime
 from dateutil import parser
@@ -19,23 +20,65 @@ def index(request):
     context = {'Years': years_list, "Semesters": semesters_list}
     return HttpResponse(template.render(context, request))
 
-def submit(request):
+def submit_s(request):
     requested_sem = request.GET['Semester'] + " " + request.GET['Year']
     try:
         chosen_sem = get_object_or_404(semester, sem_name = requested_sem)
     except:
         return HttpResponse("Not Found")
-    temp = chosen_sem.courses[0]["lectures"][0]
-    dates_list = list(set([i['date'] for i in (temp["Cap"] + temp["Reg"] + temp["With"] + temp["Combd"])]))
+    course_list = [i["cname"] for i in chosen_sem.courses]
+    course_list.insert(0, "<Please pick a Course>")
+    template = loader.get_template('course_list.html')
+    context = { 'courses': course_list}
+    return HttpResponse(template.render(context, request))
+
+def submit_s_c(request):
+    requested_course = request.GET['course']
+    requested_sem = requested_course.split()[2] + ' ' + requested_course.split()[3]
+    try:
+        chosen_sem = get_object_or_404(semester, sem_name = requested_sem)
+    except:
+        return HttpResponse("Semester Not Found")
+    course = None
+    for i in chosen_sem.courses:
+        if(requested_course == i["cname"]):
+            course = i
+    if(course == None):
+        raise Http404("Course not found")
+    lectures = [i["Lecture"] for i in course["lectures"]]
+    template = loader.get_template('lecture_list.html')
+    context = {'lectures': lectures, "course": request.GET['course']}
+    return HttpResponse(template.render(context, request))
+
+def submit_s_c_l(request):
+    requested_course = request.GET['course']
+    requested_sem = requested_course.split()[2] + ' ' + requested_course.split()[3]
+    try:
+        chosen_sem = get_object_or_404(semester, sem_name = requested_sem)
+    except:
+        return HttpResponse("Not Found")
+    course = None
+    for i in chosen_sem.courses:
+        if(requested_course == i["cname"]):
+            course = i['lectures']
+    if(course == None):
+        raise Http404("Course not found")
+    lecture = None
+    for i in course:
+        if(request.GET['lecture'] == i["Lecture"]):
+            lecture = i
+    if(lecture == None):
+        raise Http404("Lecture not found")
+    dates_list = list(set([i['date'] for i in (lecture["Cap"] + lecture["Reg"] + lecture["With"] + lecture["Combd"])]))
     dates_list_2 = [parser.parse(i) for i in dates_list]
     dates_list = [i for _,i in sorted(zip(dates_list_2, dates_list))]
     curr_date = datetime.now()
     dates_list.append(curr_date.isoformat())
-    cap_dic = {i['date']: i['num'] for i in temp["Cap"]}
-    reg_dic = {i['date']: i['num'] for i in temp["Reg"]}
-    with_dic = {i['date']: i['num'] for i in temp["With"]}
-    cur_dic = {i['date']: i['num'] for i in temp["Cur"]}
-    combd_dic = {i['date']: i['num'] for i in temp["Combd"]}
+    cap_dic = {i['date']: i['num'] for i in lecture["Cap"]}
+    reg_dic = {i['date']: i['num'] for i in lecture["Reg"]}
+    with_dic = {i['date']: i['num'] for i in lecture["With"]}
+    cur_dic = {i['date']: i['num'] for i in lecture["Cur"]}
+    combd_dic = {i['date']: i['num'] for i in lecture["Combd"]}
     for i in range(len(dates_list)):
         if cap_dic.get(dates_list[i], -1) == -1:
             cap_dic[dates_list[i]] = cap_dic[dates_list[i-1]]
