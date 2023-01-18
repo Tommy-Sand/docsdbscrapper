4cd464ce4a5ca16060a79fc839b6595b8e21e40e
Tommy-Sand <84103219+Tommy-Sand@users.noreply.github.com>
2023-01-18 09:05:13 -0700

    Update settings.py

--- enrollmentgrapher/enrollmentgrapher/settings.py
+++ enrollmentgrapher/enrollmentgrapher/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^y64agyez$(i2%=+#2!2t0+cvc)91)sd078h=u6!9ux8!k_+1'
+SECRET_KEY = 'SUPER SECRET API KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
